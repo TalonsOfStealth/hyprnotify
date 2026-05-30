8b6fd3167ab82f8c9c348b5bda75dfbc4a21aa11
Harsh Sharma <harsh@codelif.in>
2026-05-30 18:38:46 +0530

    fix: disable sound by default
    Saner default, I got sick of that sound.

--- cmd/hyprnotify/main.go
+++ cmd/hyprnotify/main.go
@@ -6,19 +6,19 @@ import (
 )
 
 func main() {
-	var disableSound bool
+	var enableSound bool
 
 	Cmd := &cobra.Command{
 		Use:  "hyprnotify",
 		Long: `DBus Implementation of Freedesktop Notification spec for 'hyprctl notify'`,
 		Run: func(cmd *cobra.Command, args []string) {
-			internal.InitDBus(!disableSound)
+			internal.InitDBus(enableSound)
 		},
 	}
 
 	CmdFlags := Cmd.Flags()
 
-	CmdFlags.BoolVarP(&disableSound, "no-sound", "s", false, "disable sound, silent mode")
+	CmdFlags.BoolVarP(&enableSound, "sound", "s", false, "enable a short notification sound")
 	CmdFlags.Uint8VarP(&internal.DefaultFontSize, "font-size", "f", 13, "set default font size (range 1-255)")
 	CmdFlags.BoolVar(&internal.FixedFontSize, "fixed-font-size", false, "makes font size fixed, ignoring new sizes")
 
